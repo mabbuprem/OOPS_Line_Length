4217d5c1eb7faa575037d345b12de2a48e6ff235
mabbuprem <mabbupremkumar@gmail.com>
2022-02-23 22:54:32 +0530

    comparing two lines

--- Program.cs
+++ Program.cs
@@ -8,11 +8,12 @@ namespace OOPS_Line_Length
         public static void Main()
         {
             Console.WriteLine("Welcome to line length program of OOPS Concept");
-            LengthOfLine lengthOfLine = new LengthOfLine(5, 4, 6, 7);
-            lengthOfLine.lineLength();
-
-
-
+            LengthOfLine lengthOfLine1 = new LengthOfLine(5, 4, 6, 7);
+            lengthOfLine1.lineLength();
+            LengthOfLine lengthOfLine2 = new LengthOfLine(7, 8, 9, 2);
+            lengthOfLine2.lineLength();
+            string result = lengthOfLine1.Equals(lengthOfLine2) == true ? "Two lines are equal" : "Two lines are not equal";
+            Console.WriteLine(result);
         }
     }
 
