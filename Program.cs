52996180f0cda516748b9f1cd34447d4bb21bad5
mabbuprem <mabbupremkumar@gmail.com>
2022-02-23 22:29:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Line_Comparison
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to line length program of OOPS Concept");
+
+
+        }
+    }
+
+}
