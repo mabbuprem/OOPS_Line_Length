30283acc2dc524f5f3738ca52d146aa0e222bf8b
mabbuprem <mabbupremkumar@gmail.com>
2022-02-23 22:36:52 +0530

    length of line

---
+++ LenghOfLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_Line_Length
+{
+    internal class LengthOfLine
+    {
+        public double valueOfX1;
+        public double valueOfX2;
+        public double valueOfY1;
+        public double valueOfY2;
+
+        public LengthOfLine(double valueOfX1, double valueOfX2, double valueOfY1, double valueOfY2)
+        {
+            this.valueOfX1 = valueOfX1;
+            this.valueOfX2 = valueOfX2;
+            this.valueOfY1 = valueOfY1;
+            this.valueOfY2 = valueOfY2;
+        }
+        public void lineLength()
+        {
+
+
+            double lengthOfALine = Math.Pow(Math.Pow(valueOfX2 - valueOfY1, 2) + Math.Pow(valueOfX2 + valueOfY1, 2), 0.5);
+            Console.WriteLine(lengthOfALine);
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 
 
-namespace Line_Comparison
+namespace OOPS_Line_Length
 {
     class Program
     {
         public static void Main()
         {
             Console.WriteLine("Welcome to line length program of OOPS Concept");
+            LengthOfLine lengthOfLine = new LengthOfLine(5, 4, 6, 7);
+            lengthOfLine.lineLength();
+
 
 
         }
